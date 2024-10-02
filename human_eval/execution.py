d0242615921d51c1fa06672def9184bdabfcddfa
Rya Sanovar <72592179+Rya-Sanovar@users.noreply.github.com>
2024-10-02 10:32:55 +0530

    exec uncommented

--- human_eval/execution.py
+++ human_eval/execution.py
@@ -63,7 +63,7 @@ def check_correctness(problem: Dict, completion: str, timeout: float,
 # Once you have read this disclaimer and taken appropriate precautions, 
 # uncomment the following line and proceed at your own risk:
                         
-                        # exec(check_program, exec_globals)
+                        exec(check_program, exec_globals)
                 result.append("passed")
             except TimeoutException:
                 result.append("timed out")
